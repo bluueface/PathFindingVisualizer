ad8b2856501738b624b4a3770a1d498f88b44d42
bluueface <azizwaadoud7@gmail.com>
2024-08-10 15:50:00 -0500

    first commit

---
+++ client/src/App.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "./App.css";
+
+const App: React.FunctionComponent = () => {
+  return (
+    <div className="app">
+      <h4>Path Finding vizualizer</h4>
+    </div>
+  );
+};
+
+export default App;
---
+++ server/app.ts
@@ -0,0 +1,12 @@
+import express, { Request, Response } from "express";
+import { PORT } from "./config";
+
+const app = express();
+
+app.get("/", (req: Request, res: Response) => {
+  res.send("Hello From Express!");
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running at http://localhost:${PORT}`);
+});
