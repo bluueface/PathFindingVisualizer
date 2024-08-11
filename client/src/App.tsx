3d5ec68103fe7059c04e1ddf622259e573f3821f
bluueface <azizwaadoud7@gmail.com>
2024-08-10 20:07:15 -0500

    tailwindCSS setup

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,10 +1,11 @@
 import React from "react";
-import "./App.css";
 
 const App: React.FunctionComponent = () => {
   return (
-    <div className="app">
-      <h4>Path Finding vizualizer</h4>
+    <div>
+      <span className="text-3xl font-bold underline">
+        Path Finding vizualizer
+      </span>
     </div>
   );
 };
